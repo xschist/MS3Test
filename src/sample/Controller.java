16484a34fa5309f8c74320a73abe00b4e9db648e
xschist <telpad99@yahoo.com>
2020-09-16 06:19:12 +0800

    -Open File Dialog thingy (will add sql writer soon)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,7 +6,13 @@ import com.univocity.parsers.csv.CsvParserSettings;
 import com.univocity.parsers.csv.CsvWriter;
 import com.univocity.parsers.csv.CsvWriterSettings;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.*;
 import java.nio.file.StandardOpenOption;
@@ -15,13 +21,15 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Controller {
 
     @FXML
+    private VBox mainBox;
+
+    @FXML
     public void uploadCSV() throws UnsupportedEncodingException, FileNotFoundException {
-
-
 
         //univocity parser settings
         CsvParserSettings settings = new CsvParserSettings();
@@ -44,20 +52,26 @@ public class Controller {
             System.out.println("read");
 
             int invalidCounter= 0;
+            int validCounter = 0;
 
             //univocity writer
             CsvWriterSettings cSettings = new CsvWriterSettings();
             CsvWriter writer = new CsvWriter(new FileWriter(new File(newFilePath)), cSettings);
 
-            //writes to _invalid records csv and logs
+
             for(String[] row : parser.iterate(reader)){
                     for(int count = 0; count<10; count++){
+
+                        //writes to _invalid records csv and logs
                         if(row[count] == null){
                             invalidCounter++;
                             System.out.println(invalidCounter+ " "+Arrays.toString(row));
                             writer.writeRow(row); break;
 
                         }
+
+                        //writes to sql database
+                        else{validCounter++;}
                     }
 
             }
@@ -65,10 +79,13 @@ public class Controller {
 
         }
         catch (FileNotFoundException e){
-            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid CSV File");
+            alert.setContentText("CSV File not Consumed");
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            alert.showAndWait();
+        } catch (Exception e) {
+            //e.printStackTrace();
         }
 
     }
@@ -80,15 +97,26 @@ public class Controller {
 
     public String openFile(){
         try{
+            Stage stage = (Stage) mainBox.getScene().getWindow();
+           // stage.initModality(Modality.WINDOW_MODAL);
             FileChooser fileChooser = new FileChooser();
             fileChooser.setTitle("Select CSV File:");
             fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("CSV Files","*.csv"));
-            File newFile = fileChooser.showOpenDialog(null);
+
+            File newFile = fileChooser.showOpenDialog(stage);
            return newFile.getAbsolutePath().replace("\\","/");}
 
         catch (Exception e) {
-            e.printStackTrace();
+
         }
         return null;
+    }
+
+    public int columnCounter(String[] row){
+        int colCount = 0;
+
+
+
+        return colCount;
     }
 }
